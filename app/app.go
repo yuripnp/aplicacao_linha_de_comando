33f06e5953fecea516cac752502cb19554dc94f0
yuripnunes <yuri.p.nunes@accenture.com>
2024-09-04 18:10:18 -0300

    busca de ips

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // vai ser em maiusculo pq eu quero que seja exportado para a main
 func Gerar() *cli.App {
@@ -8,5 +14,31 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca Ips e nomes de servidores"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca ips de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
